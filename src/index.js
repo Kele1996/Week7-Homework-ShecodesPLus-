48d63e979b9cbe412a92feb807b867f72262ccf4
Kele1996 <angelamooketsi5@gmail.com>
2025-07-03 10:13:12 +0200

    Added search functionality using the api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector(#city);
+
+cityElement.innerHtml = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+searchCity(city) {
+   let apiKey = "4115af60ae687t4b4d7440a16fdo31fe";
+   let apiUrl =  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit (event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector(#city);
-cityElement.innerHtml = searchInput.value
+
+searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector(#search-form);
 SearchFormElement.addEventListener("Submit",handleSearchSubmit);
+
+SearchCity("Pretoria");
