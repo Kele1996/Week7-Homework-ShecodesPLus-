d37056ceab2c16b19ebfc5b54d55a3680f7c90aa
Kele1996 <angelamooketsi5@gmail.com>
2025-07-08 07:34:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function refreshWeather(response) {
     let date = new Date(response.data.time * 1000);
 let iconElement = document.querySelector(#icon);
 
-iconElement.innerHTML = `<img src="${response.condition.icon_url}" class="weather-app-icon" />`;
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
 cityElement.innerHtml = response.data.city;
 
